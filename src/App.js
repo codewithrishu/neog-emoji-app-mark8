974c322fa57144ef455f84c832132ef95710f186
Rishu Jain <77292616+codewithrishu@users.noreply.github.com>
2021-08-18 23:38:20 +0530

    style did some styling

--- src/App.js
+++ src/App.js
@@ -30,25 +30,32 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Know your emotions</h1>
+      <h1>Know your Em😈ti😜ns!</h1>
+
       <input
         onChange={changeHandler}
         placeholder="search your emoji here"
       ></input>
 
-      <div style={{ padding: "1.5rem", fontSize: "1.5rem" }}>{meaning}</div>
-
-      {Object.keys(emojiDictionary).map((item) => {
-        return (
-          <span
-            onClick={() => clickHandler(item)}
-            style={{ padding: "1rem", cursor: "pointer", fontSize: "2.5rem" }}
-            key={item}
-          >
-            {item}
-          </span>
-        );
-      })}
+      <div style={{ padding: "1rem", fontSize: "1.5rem" }}>{meaning}</div>
+      <div style={{ padding: "1rem" }}>
+        {Object.keys(emojiDictionary).map((item) => {
+          return (
+            <span
+              onClick={() => clickHandler(item)}
+              style={{
+                padding: ".5rem",
+                cursor: "pointer",
+                fontSize: "2rem",
+                boxShadow: "1px 1px 2px 2px #888888"
+              }}
+              key={item}
+            >
+              {item}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
